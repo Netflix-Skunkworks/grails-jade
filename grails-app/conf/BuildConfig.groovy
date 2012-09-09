b6a52483556fc939d69b275ffb2eeee5b4b98948
Jason Gritman <jasongritman@gmail.com>
2012-09-09 12:02:26 -0700

    initial checkin

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,28 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsCentral()
+        mavenRepo "https://github.com/neuland/jade4j/raw/master/releases"
+        mavenRepo "https://github.com/neuland/spring-jade4j/raw/master/releases"
+    }
+    dependencies {
+        compile "de.neuland:spring-jade4j:0.2.2"
+    }
+
+    plugins {
+        build(":tomcat:$grailsVersion",
+              ":release:2.0.3",
+              ":rest-client-builder:1.0.2") {
+            export = false
+        }
+    }
+}
